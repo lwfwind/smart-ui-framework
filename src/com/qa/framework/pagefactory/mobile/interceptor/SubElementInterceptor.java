3b7bfa9d005a3c1e72d4f2bb6d190497fc239e03
wind <wind@abc360.com>
2016-07-05 10:10:53 +0800

    ignore object method intercept such as finalize

--- src/com/qa/framework/pagefactory/mobile/interceptor/SubElementInterceptor.java
+++ src/com/qa/framework/pagefactory/mobile/interceptor/SubElementInterceptor.java
@@ -76,6 +76,10 @@ public class SubElementInterceptor implements MethodInterceptor {
 
     public Object intercept(Object obj, Method method, Object[] args,
                             MethodProxy proxy) throws Throwable {
+        if (Object.class.equals(method.getDeclaringClass())) {
+            return proxy.invokeSuper(obj, args);
+        }
+
         if (args != null && args.length > 0) {
             logger.info(this.field.getName() + "_" + num + " " + method.getName() + " " + Arrays.deepToString(args));
         } else {
